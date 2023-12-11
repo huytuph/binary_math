2af9acf838df847d6e8488f5230c7b1fab8037ac
huytu <huytuph@gmail.com>
2023-12-11 22:17:18 +1100

    added clear_screen function

--- binary_math.py
+++ binary_math.py
@@ -2,8 +2,11 @@
 binary math tool
 """
 import binary_decimal, decimal_binary, os
-
+#
 def clear_screen():
+    """
+    clears screen for all operating systems
+    """
     if os.name == 'nt':
         os.system('cls')
     else:
@@ -12,30 +15,25 @@ def clear_screen():
 def _invalid():
     """invalid if user input is not accepted"""
     print("\n!! invalid option !!\nplease try again...\n")
-
-##
-##
-
+#
+#
 """
 Main menu for binary math tool
-Current options:
-(1) Binary to Decimal conversion
-(2) Decimal to Binary conversion
-(x) Exit 
 """
 while True:
+    clear_screen()
     print("""
->> BINARY MATH TOOL <<
-          
+>> BINARY MATH TOOL <<\n          
 (1) Binary to Decimal conversion
 (2) Decimal to Binary conversion
-(x) Exit """)
+(q) Exit """)
     option = str(input("Select an option: "))
-    if option == "x":
+    if option == "q" or option == "Q":
         exit()
         
     elif option == "1":   
         while True:
+            clear_screen()
             binary_input = input("\nEnter binary number: ")
             binary_decimal.binary_decimal(binary_input)
             opt = str(input("\nreturn to previous menu? (y/N): "))
@@ -43,10 +41,11 @@ while True:
                 continue
             elif opt == "y" or opt == "Y":
                 break
-        clear_screen()
+        #clear_screen()
         
     elif option == "2":
         while True:
+            clear_screen()
             decimal_input = int(input("\nEnter decimal number: "))
             decimal_binary.decimal_binary(decimal_input)
             opt = str(input("\nreturn to previous menu? (y/N): "))
@@ -54,9 +53,8 @@ while True:
                 continue
             elif opt == "y" or opt == "Y":
                 break
-        clear_screen()
+        #clear_screen()
     else:
         _invalid()
-
 ##
 ##
