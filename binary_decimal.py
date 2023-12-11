6ba2e4f781c9f9d02fd580d7580013c61c54ea07
huytuph <89845294+huytuph@users.noreply.github.com>
2023-12-11 17:39:09 +1100

    Update binary_decimal.py

--- binary_decimal.py
+++ binary_decimal.py
@@ -1,27 +1,36 @@
-def binary_decimal():
+def binary_decimal(binary_input):
     """
-    8 bit binary to decimal converter
+    bit count for binary input 
     """
-    octet = [128,64,32,16,8,4,2,1]
+    bin_len = len(binary_input)
+    bit_list = []
+    bits = 1
+    
     binary = []
     decimal = []
     decimal_sum = 0
-    binary_input = input("Enter binary number(max 8digits): ")
     
-    if len(binary_input) <= 8:
-        for num in binary_input:
-            binary.append(int(num))
-        count = 0
-        while count != 8:
-            for x in binary:
-                if x != 0:
-                    decimal.append(octet[count])
-                    count += 1
-                else:
-                    decimal.append(0) 
-                    count += 1
+    while bin_len != 0:
+        bit_list.append(bits)
+        bits *= 2
+        bin_len -= 1
+        
+    bit_list.reverse()
+
+    for num in binary_input:
+        binary.append(int(num))
+    count = 0
+    while count != len(bit_list):
+        for x in binary:
+            if x != 0:
+                decimal.append(bit_list[count])
+                count += 1
+            else:
+                decimal.append(0)
+                count += 1
         for dec in decimal:
             decimal_sum += dec
-        print(f"Decimal number: {decimal_sum}")    
-    else:
-        print("invalid")
+    print(f"Decimal number: {decimal_sum}")    
+    
+binary_input = input("Enter binary number: ")
+binary_decimal(binary_input)
