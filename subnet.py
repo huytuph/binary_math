68a9dea93ffa6997663c66a1ff6d10b5aa175a38
huytuph <89845294+huytuph@users.noreply.github.com>
2023-12-15 01:11:40 +1100

    Update

---
+++ subnet.py
@@ -0,0 +1,12 @@
+# usable host per subnet
+
+255 255 000 000
+multiply octets with octets then
+-2 #[network address, broadcast address]
+= available hosts
+
+subnet_list = []
+host = []
+host = 0
+count = 0
+for x in subnet_list[]:
