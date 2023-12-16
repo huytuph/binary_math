f00298ab3faeb8d9bf73df4d7f2b154e97642b67
huytuph <89845294+huytuph@users.noreply.github.com>
2023-12-16 20:50:12 +1100

    Update subnet.py

--- subnet.py
+++ subnet.py
@@ -4,7 +4,7 @@ usable host per subnet
 """
 subm_in = input("enter subnet mask: ") # user input
 subm = subm_in.split('.') # octet list w/out '.'
-subnet = []    # octet list as int
+subnet = [] # octet list as int
 hosts = 1 # total host available on network
 for x in subm:
     subnet.append(int(x)) # add to subnet list as int
